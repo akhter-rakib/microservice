65bf28acd7de0d5493e8999b7d62cf09c0960b8a
akter_rakib <rakibccj@gmail.com>
2019-10-06 09:53:55 +0600

    initiate three service

---
+++ microservice/movie-info-service/src/main/java/ms/rakib/movieinfoservice/model/Movie.java
@@ -0,0 +1,28 @@
+package ms.rakib.movieinfoservice.model;
+
+public class Movie {
+
+    private String modieId;
+    private String name;
+
+    public Movie(String modieId, String name) {
+        this.modieId = modieId;
+        this.name = name;
+    }
+
+    public String getModieId() {
+        return modieId;
+    }
+
+    public void setModieId(String modieId) {
+        this.modieId = modieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
