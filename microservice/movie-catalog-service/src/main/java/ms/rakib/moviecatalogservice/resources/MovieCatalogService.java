25bca546f234a07cd9ce4616e80733892ad4b784
akter_rakib <rakibccj@gmail.com>
2019-10-16 23:09:05 +0600

    ignore returning list

--- microservice/movie-catalog-service/src/main/java/ms/rakib/moviecatalogservice/resources/MovieCatalogService.java
+++ microservice/movie-catalog-service/src/main/java/ms/rakib/moviecatalogservice/resources/MovieCatalogService.java
@@ -3,6 +3,7 @@ package ms.rakib.moviecatalogservice.resources;
 import ms.rakib.moviecatalogservice.model.CatalogItem;
 import ms.rakib.moviecatalogservice.model.Movie;
 import ms.rakib.moviecatalogservice.model.Rating;
+import ms.rakib.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,21 +19,17 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogService {
     //get all rated movie id
-    //for each movie id, call movie info and get details
-    //put them all together
     @Autowired
     public RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1", 4),
-                new Rating("2", 3)
-        );
-
-        return ratings.stream().map(rating -> {
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        return ratings.getUserRating().stream().map(rating -> {
+            //for each movie id, call movie info and get details
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            //put them all together
             return new CatalogItem(movie.getName(), "Desktop", rating.getRating());
         }).collect(Collectors.toList());
     }
--- microservice/ratings-data-service/src/main/java/ms/rakib/ratingsdataservice/resources/RatingResource.java
+++ microservice/ratings-data-service/src/main/java/ms/rakib/ratingsdataservice/resources/RatingResource.java
@@ -1,9 +1,13 @@
 package ms.rakib.ratingsdataservice.resources;
 
 import ms.rakib.ratingsdataservice.model.Rating;
+import ms.rakib.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,6 +16,17 @@ public class RatingResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getRatingList(@PathVariable("userId") String userId) {
+        List<Rating> ratingList = Arrays.asList(
+                new Rating("123", 4),
+                new Rating("123", 5)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratingList);
+        return userRating;
     }
 }
 
